afd6040aa0307bd94658d0cac875442b5b7ef617
hhw12409 <sksk2425@naver.com>
2022-12-02 02:43:28 +0900

    style: delete styled-component

--- src/components/MenuTab/MenuTab.styles.tsx
+++ src/components/MenuTab/MenuTab.styles.tsx
@@ -12,16 +12,3 @@ export const styles = StyleSheet.create({
     marginTop: 10,
   },
 });
-
-import styled from 'styled-components/native';
-
-const IconImg = styled.Image`
-  width: 24px;
-  height: 24px;
-`;
-
-const TabName = styled.Text`
-  margin-top: 10px;
-`;
-
-export { IconImg, TabName };

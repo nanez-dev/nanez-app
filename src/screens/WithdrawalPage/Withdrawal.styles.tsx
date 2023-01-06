3ab78422d17f6e779dc83e379c1810b970765073
hhw12409 <sksk2425@naver.com>
2023-01-06 20:25:33 +0900

    style: withdrawal page button styling

--- src/screens/WithdrawalPage/Withdrawal.styles.tsx
+++ src/screens/WithdrawalPage/Withdrawal.styles.tsx
@@ -4,8 +4,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  headerWrap: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  subTitle: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  withDrawalWrap: {
+    width: '80%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#65bfc4',
+    borderRadius: 50,
+  },
+  withDrawalTitle: {
+    fontSize: 30,
+    fontWeight: '600',
+    color: 'white',
   },
 });
 
--- src/screens/WithdrawalPage/Withdrawal.tsx
+++ src/screens/WithdrawalPage/Withdrawal.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
-import RegisterHeader from '../../components/@shared/RegisterHeader/RegisterHeader';
 import { styles } from './Withdrawal.styles';
 
 const Withdrawal = () => {
   return (
     <View style={styles.container}>
-      <RegisterHeader title="회원탈퇴" subtitle="탈퇴를 희망하시면 탈퇴하기를 눌러주세요." />
-      <TouchableOpacity>
-        <Text>탈퇴하기</Text>
+      <View style={styles.headerWrap}>
+        <Text style={styles.title}>회원탈퇴</Text>
+        <Text style={styles.subTitle}>탈퇴를 희망하시면 탈퇴하기를 눌러주세요.</Text>
+      </View>
+      <TouchableOpacity style={styles.withDrawalWrap}>
+        <Text style={styles.withDrawalTitle}>탈퇴하기</Text>
       </TouchableOpacity>
     </View>
   );
